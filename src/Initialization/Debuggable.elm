78d6b6180fbdecc9992888bc5ed86b02c25f8c08
Morinobu Kuzunuki <morinobukuzunuki@gmail.com>
2025-12-29 18:07:35 +0900

    changed argument of TransformingFuncionArrived to record and added doc comments

--- src/Initialization/Debuggable.elm
+++ src/Initialization/Debuggable.elm
@@ -1,64 +1,46 @@
-module Initialization.Debuggable exposing (Program, accumulate, element, toDebuggable, toDebuggableCmd)
+module Initialization.Debuggable exposing (Program, accumulate, document, element, toDebuggable, toDebuggableCmd)
 
-import Html exposing (Html)
-import Initialization.Advanced as I
-import Task
+{-| This module enables you to prepare more precise debugging information
+than the `Initialization` module.
 
+The information provided by this module is available only when you compile
+your application with `elm make --debug`.
 
-type MsgWithInfo initUpdateInfo initializingModel
-    = TransformFunctionArrived String ({ old : initializingModel, new : initializingModel } -> initUpdateInfo) (initializingModel -> initializingModel)
-    | TransformExecuted
-        { description : String
-        , updateInfo : initUpdateInfo
-        }
+When you use the `Initialization` module, the information shown in the Elm
+Debugger during the initializing phase is often surprisingly poor.
+This is because all `Msg` values in that phase are model-transforming functions,
+which the debugger can only display as `<internals>`.
 
+By using this module, you can:
 
-type alias Program flag initializingModel initUpdateInfo runningModel runningMsg =
-    I.Program flag initializingModel (MsgWithInfo initUpdateInfo initializingModel) runningModel runningMsg
-
-
-initUpdate : MsgWithInfo initUpdateInfo initializingModel -> initializingModel -> ( initializingModel, Cmd (MsgWithInfo initUpdateInfo initializingModel) )
-initUpdate iMsg iModel =
-    case iMsg of
-        TransformFunctionArrived description toUpdateInfo transform ->
-            let
-                newModel =
-                    transform iModel
-            in
-            ( newModel
-            , Task.perform identity
-                (Task.succeed
-                    (TransformExecuted
-                        { description = description
-                        , updateInfo = toUpdateInfo { old = iModel, new = newModel }
-                        }
-                    )
-                )
-            )
-
-        TransformExecuted _ ->
-            ( iModel, Cmd.none )
+  - add `String` descriptions to each model-transforming function returned by commands
+  - inspect additional information when a transformation is executed, based on the `initializingModel` values before and after the transformation
 
 
-toDebuggableCmd :
-    String
-    -> ({ old : initializingModel, new : initializingModel } -> initUpdateInfo)
-    -> Cmd (initializingModel -> initializingModel)
-    -> Cmd (MsgWithInfo initUpdateInfo initializingModel)
-toDebuggableCmd description toUpdateInfo =
-    Cmd.map (TransformFunctionArrived description toUpdateInfo)
+# How to Migrate from the `Initialization` Module
+
+1.  Change `import Initialization as Init`
+    to `import Initialization.Debuggable as Init`.
+    Using the same alias saves time and effort.
+
+2.  Add `Init.toDebuggableCmd "" (always Nothing)`
+    before each `Cmd` value passed to `Cmd.batch` in your `init` function.
+
+3.  Add `Init.toDebuggable "" (always Nothing)`
+    before each model-transforming function in the `Html` value returned
+    from the `initView` function.
+
+4.  Fill in the empty string arguments (`""`) with descriptions as needed.
+
+5.  Replace any of the `(always Nothing)` functions with
+    `({ old : initializingModel, new : initializingModel } -> updateInfo)`,
+    where `updateInfo` is any type you define.
+    Wrapping the value in `Just` is usually sufficient.
 
 
-toDebuggable :
-    String
-    -> ({ old : initializingModel, new : initializingModel } -> initUpdateInfo)
-    -> (initializingModel -> initializingModel)
-    -> MsgWithInfo initUpdateInfo initializingModel
-toDebuggable =
-    TransformFunctionArrived
+# Example Code
 
-
-{-|
+The code below demonstrates how to debug Posix time initialization.
 
     import Html
     import Initialization.Debuggable as Init
@@ -66,8 +48,7 @@ toDebuggable =
     import Time
 
     type InitUpdateInfo
-        = NoInfo
-        | PosixTimeUpdated { old : Maybe Time.Posix, new : Maybe Time.Posix }
+        = PosixTimeUpdated { old : Maybe Time.Posix, new : Maybe Time.Posix }
 
     type Msg
         = Tick Time.Posix
@@ -81,8 +62,8 @@ toDebuggable =
                       , mPosixTime = Nothing
                       }
                     , Cmd.batch
-                        [ Init.toDebuggableCmd "updating timezone"
-                            (\_ -> NoInfo)
+                        [ Init.toDebuggableCmd ""
+                            (always Nothing)
                             (Task.perform (\tz im -> { im | mTimezone = Just tz }) Time.here)
                         , Init.toDebuggableCmd "updating posix time"
                             (\{ old, new } -> PosixTimeUpdated { old = old.mPosixTime, new = new.mPosixTime })
@@ -117,6 +98,102 @@ toDebuggable =
             }
 
 -}
+
+import Browser
+import Html exposing (Html)
+import Initialization.Advanced as I
+import Task
+
+
+{-| `Msg` type used internally to represent model-transforming steps during the initializing phase.
+-}
+type MsgWithInfo initUpdateInfo initializingModel
+    = TransformFunctionArrived
+        { description : String
+        , toUpdateInfo : { old : initializingModel, new : initializingModel } -> initUpdateInfo
+        , transform : initializingModel -> initializingModel
+        }
+    | TransformExecuted
+        { description : String
+        , updateInfo : initUpdateInfo
+        }
+
+
+{-| Type alias for `Platform.Program flag <internally defined Model type> <internally defined Msg type>`.
+-}
+type alias Program flag initializingModel initUpdateInfo runningModel runningMsg =
+    I.Program flag initializingModel (MsgWithInfo initUpdateInfo initializingModel) runningModel runningMsg
+
+
+initUpdate : MsgWithInfo initUpdateInfo initializingModel -> initializingModel -> ( initializingModel, Cmd (MsgWithInfo initUpdateInfo initializingModel) )
+initUpdate iMsg iModel =
+    case iMsg of
+        TransformFunctionArrived { description, toUpdateInfo, transform } ->
+            let
+                newModel =
+                    transform iModel
+            in
+            ( newModel
+            , Task.perform identity
+                (Task.succeed
+                    (TransformExecuted
+                        { description = description
+                        , updateInfo = toUpdateInfo { old = iModel, new = newModel }
+                        }
+                    )
+                )
+            )
+
+        TransformExecuted _ ->
+            ( iModel, Cmd.none )
+
+
+{-| Converts `Cmd (initializingModel -> initializingModel)`
+into `Cmd (MsgWithInfo updateInfo initializingModel)`.
+
+This function is intended to be used in the `init` function.
+
+-}
+toDebuggableCmd :
+    String
+    -> ({ old : initializingModel, new : initializingModel } -> initUpdateInfo)
+    -> Cmd (initializingModel -> initializingModel)
+    -> Cmd (MsgWithInfo initUpdateInfo initializingModel)
+toDebuggableCmd description toUpdateInfo =
+    Cmd.map
+        (\transform ->
+            TransformFunctionArrived
+                { description = description
+                , toUpdateInfo = toUpdateInfo
+                , transform = transform
+                }
+        )
+
+
+{-| Converts `initializingModel -> initializingModel`
+into `MsgWithInfo updateInfo initializingModel`.
+
+This function is intended to be used in the `initView` function.
+
+-}
+toDebuggable :
+    String
+    -> ({ old : initializingModel, new : initializingModel } -> initUpdateInfo)
+    -> (initializingModel -> initializingModel)
+    -> MsgWithInfo initUpdateInfo initializingModel
+toDebuggable description toUpdateInfo transform =
+    TransformFunctionArrived
+        { description = description
+        , toUpdateInfo = toUpdateInfo
+        , transform = transform
+        }
+
+
+{-| Similar to `Initialization.element`, but requires that all `Msg` values
+are converted from `initializingModel -> initializingModel`
+into `MsgWithInfo initUpdateInfo initializingModel`
+using `toDebuggableCmd` and `toDebuggable`.
+-}
 element :
     { init : flag -> ( initializingModel, Cmd (MsgWithInfo initUpdateInfo initializingModel) )
     , initView : List blockingReason -> initializingModel -> Html (MsgWithInfo initUpdateInfo initializingModel)
@@ -138,6 +215,34 @@ element { init, initView, toRunning } =
         }
 
 
+{-| Similar to `Initialization.document`, but requires that all `Msg` values
+are converted from `initializingModel -> initializingModel`
+into `MsgWithInfo initUpdateInfo initializingModel`
+using `toDebuggableCmd` and `toDebuggable`.
+-}
+document :
+    { init : flag -> ( initializingModel, Cmd (MsgWithInfo initUpdateInfo initializingModel) )
+    , initView : List blockingReason -> initializingModel -> Browser.Document (MsgWithInfo initUpdateInfo initializingModel)
+    , toRunning : initializingModel -> Result (List blockingReason) ( runningModel, Cmd runningMsg )
+    }
+    ->
+        { subscriptions : runningModel -> Sub runningMsg
+        , update : runningMsg -> runningModel -> ( runningModel, Cmd runningMsg )
+        , view : runningModel -> Browser.Document runningMsg
+        }
+    -> Program flag initializingModel initUpdateInfo runningModel runningMsg
+document { init, initView, toRunning } =
+    I.document
+        { init = init
+        , initSubscriptions = \_ -> Sub.none
+        , initUpdate = initUpdate
+        , initView = initView
+        , toRunning = toRunning
+        }
+
+
+{-| The same as `Initialization.accumulate`.
+-}
 accumulate : Result blockingReason a -> Result (List blockingReason) (a -> b) -> Result (List blockingReason) b
 accumulate =
     I.accumulate
